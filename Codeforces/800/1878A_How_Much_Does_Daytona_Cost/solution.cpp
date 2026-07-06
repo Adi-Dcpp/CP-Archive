b94ac94de8c345ca73735d4977c479321f8f634d
Adi <luffy1120adi@gmail.com>
2026-07-07 00:45:20 +0530

    1878A

---
+++ Codeforces/800/1878A_How_Much_Does_Daytona_Cost/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+static inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main() {
+    fast_io();
+
+    int T = 1;
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        // Solve here.
+    }
+    return 0;
+}
